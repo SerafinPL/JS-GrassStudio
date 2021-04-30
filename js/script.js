2f3aa69638e79cfe66cd71a5fddd2fd25e31c880
serafinpl <ser.burberibar@gmail.com>
2021-04-30 02:07:54 +0200

    hover scripts are done

--- js/script.js
+++ js/script.js
@@ -9,13 +9,15 @@ function showDropDown() {
 function hideDropDown() {
 	// body...
 
-	this.children[1].style.display = 'block';
+	this.children[1].style.display = 'none';
+	
 }
 
 let elements = document.getElementsByClassName('haveDropdown');//.addEventListener('mouseover', showDropDown);
 
 for (element of elements) {
 	element.addEventListener('mouseover', showDropDown);
+	element.addEventListener('mouseleave', hideDropDown);
 
 }
 
